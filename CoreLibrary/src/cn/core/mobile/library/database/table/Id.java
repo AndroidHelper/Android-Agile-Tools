312e24a4a4e5e7e618c0eac6124b8c4f542c7c19
JasonZhang <zdl_411437734@hotmail.com>
2014-09-26 14:38:09 +0800

    Revert "第一版"
    This reverts commit 6f0a3a65aabbe5ae263737d9ebceba1a9928819e.

--- CoreLibrary/src/cn/core/mobile/library/database/table/Id.java
+++
@@ -1,7 +0,0 @@
-package cn.core.mobile.library.database.table;
-
-/**
- * Created by dallas on 14-4-13.
- */
-public class Id extends Property {
-}
--- CoreLibrary/src/cn/core/mobile/library/securepay/OnPayResultListener.java
+++
@@ -1,9 +0,0 @@
-package cn.core.mobile.library.securepay;
-
-/**
- * @author Will.Wu </br> Create at 2014/5/22
- * @version 1.0
- */
-public interface OnPayResultListener {
-    public void onPayResult(Throwable e);
-}
